53e7ee47545389e77673cd4ffd8e8edc5d028d1a
João Pedro Queiroz <joao.queirozst@gmail.com>
2026-06-02 13:58:55 -0300

    Create consume_data.py

---
+++ src/example/consume_data.py
@@ -0,0 +1,26 @@
+from is_wire.core import Channel, Subscription
+from is_msgs.image_pb2 import ObjectAnnotations
+import socket
+
+# Create a channel and a subscription for replies
+broker_uri = "amqp://guest:guest@10.10.2.211:30000"
+channel = Channel(broker_uri)
+subscription = Subscription(channel)
+
+# Receives rendered images from any camera.
+subscription.subscribe("tiffanyDetector.1.Detection")
+# print("Waiting images...")
+
+while True:
+    try:
+        msg = channel.consume(timeout=5.0) # Please wait for a message for up to 5 seconds.
+        detections = msg.unpack(ObjectAnnotations)
+
+        print("New detection")
+        print("Topic:", msg.topic)
+        print(detections)
+
+        print("Objects detected: ", len(detections.objects))
+      
+    except socket.timeout:
+        print("No reply.")
